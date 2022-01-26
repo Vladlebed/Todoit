f2d614b071480fcc9cff77d784b6242458e40f9e
Vladislav Skabeev <vladskab@mail.ru>
2022-01-26 10:32:54 +0400

    create a utility to convert data to a server-side format

--- src/_utils/database.js
+++ src/_utils/database.js
@@ -1,3 +1,5 @@
+import { cloneDeep } from 'lodash';
+
 export const convertDatabaseListToClientFormat = (obj, nesting = []) => Object.keys(obj || {})
   .map((key) => {
     const result = {
@@ -11,3 +13,19 @@ export const convertDatabaseListToClientFormat = (obj, nesting = []) => Object.k
 
     return result;
   });
+
+export const convertListToDatabaseFormat = (array, nesting = []) => {
+  const _array = cloneDeep(array);
+  const result = {};
+  const nextNesting = nesting.filter((el, i) => i !== 0);
+
+  _array.forEach((obj) => {
+    result[obj.uid] = obj.data;
+
+    if (nesting.length) {
+      result[obj.uid][nesting[0]] = convertListToDatabaseFormat(obj.data[nesting[0]], nextNesting);
+    }
+  });
+
+  return result;
+};
