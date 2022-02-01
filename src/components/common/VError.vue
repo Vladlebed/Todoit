1a67cd939b7b55f7f860f90f5a5116de7287fe79
Vladislav Skabeev <vladskab@mail.ru>
2022-02-01 15:03:22 +0400

    create VError

---
+++ src/components/common/VError.vue
@@ -0,0 +1,30 @@
+<template>
+  <div :class="centredClasses">
+    <v-alert type="error">
+      <slot />
+    </v-alert>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VError',
+
+  props: {
+    centred: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  computed: {
+    centredClasses() {
+      return this.centred ? 'd-flex justify-center align-center fill-height' : null;
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
