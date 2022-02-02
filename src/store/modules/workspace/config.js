f7b0cbd54ce94108ed1222ea7fbdb15f7890e69a
Vladislav Skabeev <vladskab@mail.ru>
2022-02-02 10:02:27 +0400

    add urlFactory

--- src/store/modules/workspace/config.js
+++ src/store/modules/workspace/config.js
@@ -1,5 +1,20 @@
 import { defaultsDeep } from 'lodash';
 
+export const urlFactory = {
+  WORKSPACE: (userUid, workspaceUid) => `/users/${userUid}/workspaces/${workspaceUid}`,
+  WORKSPACE_LIST: (userUid) => `/users/${userUid}/workspaces`,
+  WORKSPACE_PROPERTIES: (userUid, workspaceUid) => `/users/${userUid}/workspaces/${workspaceUid}/properties`,
+  WORKSPACE_CURRENT: (userUid) => `/users/${userUid}/currentWorkspace`,
+
+  COLUMN: (userUid, workspaceUid, columnUid) => `${urlFactory.WORKSPACE(userUid, workspaceUid)}/columns/${columnUid}`,
+  COLUMN_LIST: (userUid, workspaceUid) => `${urlFactory.WORKSPACE(userUid, workspaceUid)}/columns`,
+  COLUMN_PROPERTIES: (userUid, workspaceUid, columnUid) => `${urlFactory.WORKSPACE(userUid, workspaceUid)}/columns/${columnUid}/properties`,
+
+  CARD: (userUid, workspaceUid, columnUid, cardUid) => `${urlFactory.COLUMN(userUid, workspaceUid, columnUid)}/cards/${cardUid}`,
+  CARD_LIST: (userUid, workspaceUid, columnUid) => `${urlFactory.COLUMN(userUid, workspaceUid, columnUid)}/cards`,
+  CARD_PROPERTIES: (userUid, workspaceUid, columnUid, cardUid) => `${urlFactory.COLUMN(userUid, workspaceUid, columnUid)}/cards/${cardUid}/properties`,
+};
+
 const defaultProperties = (properties = {}) => defaultsDeep(properties, {
   name: '',
   description: '',
